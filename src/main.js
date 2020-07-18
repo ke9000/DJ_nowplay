4a3a8c0f7e245674721ff0ebbd03afa137486c14
ke9000 <odakyu9000.8000ke@gmail.com>
2020-07-19 00:04:48 +0900

    js整理した

--- src/main.js
+++ src/main.js
@@ -1,47 +1,60 @@
 function click_btn(num) {
+
+	//hashtag
 	let hash_tag1 = document.getElementById("hash_tag1");
 	let hash_tag2 = document.getElementById("hash_tag2");
-	let table = document.getElementById("tb1");
 
 	let hash_tag1_text = hash_tag1.value;
 	let hash_tag2_text = hash_tag2.value;
-	let row = table.rows[num];
-	let song_name = row.cells[1].children[0].value;
-	if(song_name){
-		song_name = song_name+"/";
-	}
-	let singer= row.cells[2].children[0].value;
-	if(singer){
-		singer = singer+"/";
-	}
-	let product = row.cells[3].children[0].value;
-	if(product){
-		product = product;
-	}
-	let other = row.cells[4].children[0].value;
-
+	
 	hash_tag1_text = hash_tag1_text.replace(/#/g, '');
 	hash_tag2_text = hash_tag2_text.replace(/#/g, '');
-	song_name = song_name.replace(/&/g, '＆');
-	singer = singer.replace(/&/g, '＆');
-	product = product.replace(/&/g, '＆');
+	
 	let hashtag_join = hash_tag1_text;
 	if(hash_tag1_text && hash_tag2_text){
 		hashtag_join = hash_tag1_text+","+hash_tag2_text;
 	} else if(hash_tag2_text) {
 		hashtag_join = hash_tag2_text;
 	}
+	
+	//tb1_general
+	let table = document.getElementById("tb1");
+	let row = table.rows[num];
+
+	//song_name
+	let song_name = row.cells[1].children[0].value;
+	if(song_name){
+		song_name = song_name+"/";
+	}
+	song_name = song_name.replace(/&/g, '＆');
+
+	//singer_name
+	let singer= row.cells[2].children[0].value;
+	if(singer){
+		singer = singer+"/";
+	}
+	singer = singer.replace(/&/g, '＆');
+
+	//product
+	let product = row.cells[3].children[0].value;
+	if(product){
+		product = product;
+	}
+	product = product.replace(/&/g, '＆');
+
+	//other
+	let other = row.cells[4].children[0].value;
 	other = other.replace(/&/g, '＆');
 	other = other.replace(/\r\n/g, '\n');
 	other = other.replace(/\r/g, '\n');
 	let other_lines = other.split('\n');
 	let other_text = other_lines.join('%0A');
 
-	console.log("No."+num+"\n"+song_name+"\n"+singer+"\n"+product+"\n\n"+other_text+"\n"+hash_tag1_text+"\n"+hash_tag2_text);
+	//output
+	console.log("No."+num+"/"+song_name+singer+product+hashtag_join+"%0A"+other_text); //debug
 
-	//text without tags
 	let join_text = "No."+num+"/"+song_name+singer+product+"%0A"+other_text;
-	console.log(join_text.length);
+	console.log(join_text.length);//debug
 
 	if(join_text.length <= 144){
 		let url = "https://twitter.com/share?text="+join_text+"&hashtags="+hashtag_join+"&url=%0A";
