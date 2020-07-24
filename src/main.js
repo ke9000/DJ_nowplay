7ff39792193bbdc6eb6fc6a367eaa8f3a5a0d360
ke9000 <odakyu9000.8000ke@gmail.com>
2020-07-25 02:58:18 +0900

    UX改善とcreatehashtagむだだったの直した

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-//ハッシュタグ登録用
-hashtags = new Array();
+//ハッシュタグ管理用
 hashtag_join = "";
 arrow_post = 1;
 //テキスト長確認用
-check_text = "";
+check_text_length = "";
 
 function click_btn(num) {
 	//hashtag
@@ -13,14 +12,17 @@ function click_btn(num) {
 	hash_tag1 = hash_tag1.replace(/#/g, '');
 	hash_tag2 = hash_tag2.replace(/#/g, '');
 
-	if(!hash_tag1){
-		alert("かならずタグ1は入力してください");
+	if(hash_tag1.match(/\s/g) || hash_tag2.match(/\s/g) ){
+		alert("ハッシュタグにはスペースを入れないで下さい");
 		arrow_post = 0;
 	}
-	hashtags.push(hash_tag1);
 
-	if(hash_tag2){
-		hashtags.push(hash_tag2);
+	if(hash_tag1&&hash_tag2){
+		hashtag_join = hash_tag1+"," + hash_tag2;
+	} else if(hash_tag1){
+		hashtag_join = hash_tag1;
+	} else if(hash_tag2){
+		hashtag_join = hash_tag2;
 	}
 	
 	//tb1_general
@@ -30,26 +32,24 @@ function click_btn(num) {
 	//song_name
 	let song_name = row.cells[1].children[0].value;
 	if(song_name){
-		song_name = check_hashtag(song_name);
-		song_name = song_name+"/";
-		check_text += song_name;
+		song_name = check_text(song_name)+"/";
+		check_text_length += song_name;
 	}
 	song_name = fixedEncodeURIComponent(song_name);
 
 	//singer_name
 	let singer= row.cells[2].children[0].value;
 	if(singer){
-		singer = check_hashtag(singer);
-		singer = singer+"/";
-		check_text += singer;
+		singer = check_text(singer)+"/";
+		check_text_length += singer;
 	}
 	singer = fixedEncodeURIComponent(singer);
 
 	//product
 	let product = row.cells[3].children[0].value;
 	if(product){
-		product = check_hashtag(product);
-		check_text += product;
+		product = check_text(product);
+		check_text_length += product;
 	}
 	product = fixedEncodeURIComponent(product);
 
@@ -57,32 +57,25 @@ function click_btn(num) {
 	let other = row.cells[4].children[0].value;
 	other = other.replace(/\r\n/g, '\n');
 	other = other.replace(/\r/g, '\n');
-	other = check_hashtag(other);
-	check_text += '\n'+other;
+	check_text_length += '\n'+other;
 	other = fixedEncodeURIComponent(other);
 	let other_text = other.split('\n').join('%0A');
 	
 	//output
-	create_hashtag(hashtags)
-	console.log("No."+num+"/"+check_text); //debug
+	console.log("No."+num+"/"+check_text_length); //debug
 
 	let join_text = "No."+num+"/"+song_name+singer+product+"%0A"+other_text;
-	console.log(check_text.length);//debug
+	console.log(check_text_length.length);//debug
 
-	if(check_text.length <= 144 && arrow_post == 1){
+	if(check_text_length.length <= 144 && arrow_post == 1){
 		let url = "https://twitter.com/share?text="+join_text+"&hashtags="+hashtag_join+"&url=%0A";
 		window.open(url, 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
-	} else if(arrow_post == 0) {
-		alert("ハッシュタグに関連して入力エラーがあります。修正してください。");
-		arrow_post = 0;
-	} else {
+	} else  if (check_text_length.length > 144){
 		alert("投稿可能文字数を超過しています。文字数を減らしてください");
 	}
-
-	//初期化
-	hashtags = [];
 	hashtag_join = "";
-	check_text = "";
+	check_text_length = "";
+	arrow_post = 1;
 }
 
 function addrow(){
@@ -123,6 +116,13 @@ function fixedEncodeURIComponent(str) {
 	});
 }
 
+function check_text(str){
+	if(str.match(/\//g)){
+		str = str.replace(/\//g, '\"\/\"');
+	}
+	return str;
+}
+
 function check_hashtag(str){
 	let tag1 = str.match(/^#.+/g);
 	if(tag1){
@@ -137,13 +137,6 @@ function check_hashtag(str){
 		}
 	}
 	return str.replace(/\s#\S+/g, ' ');
-}
-
-function create_hashtag(data){
-	hashtag_join = data[0];
-	for(i=1; i<data.length; i++){
-		hashtag_join += ","+data[i];
-	}
 }
 
 function create_url(){
